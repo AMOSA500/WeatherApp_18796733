ab67e39d9657ee83d5e7756556a7b86ee3467291
amosa500 <naf.amosa@gmail.com>
2025-11-26 10:33:50 +0000

    work on forecastview, and display map by calling the default location

---
+++ WeatherDashboardTemplate/Model/Forecast.swift
@@ -0,0 +1,8 @@
+//
+//  Forecast.swift
+//  WeatherDashboardTemplate
+//
+//  Created by Nafiu Amosa on 19/11/2025.
+//
+
+import Foundation
---
+++ WeatherDashboardTemplate/ViewModel/ForecastViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ForecastViewModel.swift
+//  WeatherDashboardTemplate
+//
+//  Created by Nafiu Amosa on 19/11/2025.
+//
+
+import Foundation
